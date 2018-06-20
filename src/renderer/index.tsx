e31dbd6bc73e523183d5a35b40caabcb348ff315
naoki-tomita <naoki.tomita@jrits.ricoh.co.jp>
2018-06-21 00:06:14 +0900

    Add app bone.

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -2,32 +2,51 @@ import * as React from "react";
 import { render } from "react-dom";
 import styled, { injectGlobal } from "styled-components";
 
-injectGlobal`
-  :not(input):not(textarea),
-  :not(input):not(textarea)::after,
-  :not(input):not(textarea)::before {
-      -webkit-user-select: none;
-      user-select: none;
-      cursor: default;
+class Headers extends React.Component {
+  render() {
+    return <div>Header</div>
   }
-  input, button, textarea, :focus {
-      outline: none;
+}
+
+class FolderList extends React.Component {
+  render() {
+    return <div>FolderList</div>;
   }
-`;
+}
+
+class Body extends React.Component {
+  render() {
+    return (
+      <>
+        <div>Body</div>
+        <ImageView/>
+      </>
+    );
+  }
+}
+
+class Footer extends React.Component {
+  render() {
+    return <div>Footer</div>
+  }
+}
+
+class ImageView extends React.Component {
+  render() {
+    return <div>ImageView</div>
+  }
+}
 
 class App extends React.Component {
   render() {
-    const Button = styled.button`
-      background-color: #123;
-      color: white;
-      font: 3em sans-serif;
-      box-shadow: 0px 5px 5px black;
-      border-color: white;
-      &:active {
-        box-shadow: 0px 1px 1px black;
-      }
-    `;
-    return <Button>Hello React World.</Button>;
+    return (
+      <>
+        <Headers/>
+        <FolderList/>
+        <Body/>
+        <Footer/>
+      </>
+    );
   }
 }
 
